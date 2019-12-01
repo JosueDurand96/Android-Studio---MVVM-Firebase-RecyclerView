566eb4737500481b756113372d1db4e11ba954a5
JosueDurand96 <josue.durand8@gmail.com>
2019-12-01 15:46:52 -0500

    mainadapter

--- app/src/main/java/com/example/recyclerviewmvvmfirebase/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewmvvmfirebase/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.recyclerviewmvvmfirebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MainAdapter()
+
     }
 }
--- app/src/main/java/com/example/recyclerviewmvvmfirebase/MainAdapter.kt
+++ app/src/main/java/com/example/recyclerviewmvvmfirebase/MainAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.recyclerviewmvvmfirebase
 
-class MainAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainAdapter(private val context: Context) :
+    RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_row, parent, false)
+
+        return MainViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+
+    }
+
+    inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
 }
